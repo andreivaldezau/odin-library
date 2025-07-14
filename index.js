21b2c97244d3bdeded3eed8d434299627c1173de
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-07-14 23:29:00 +1000

    Add library cards and styles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const container = document.querySelector(".container");
 
 function Book(title, author, pages, is_read) {
   if (!new.target) {
@@ -22,8 +23,45 @@ function addBookToLibrary(title, author, pages, is_read) {
   myLibrary.push(book);
 }
 
+function addCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const title = document.createElement("h2");
+  title.textContent = `${book.title}`;
+  const author = document.createElement("p");
+  author.textContent = `${book.author}`;
+  author.classList.add("italic");
+  const details = document.createElement("p");
+  details.textContent = `${book.pages} pages`;
+  if (book.is_read === true) {
+    card.classList.add("read");
+  } else {
+    card.classList.add("not-read");
+  }
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(details);
+  return container.appendChild(card);
+}
+
+function addBookButton() {
+  const card = document.createElement("button");
+  card.classList.add("card", "add");
+  card.textContent = "Add Book";
+  return container.appendChild(card);
+}
+
+function fillLibrary() {
+  myLibrary.forEach((book) => {
+    addCard(book);
+  });
+  addBookButton();
+}
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, true);
 addBookToLibrary("The Brothers Karamazov", "Fyodor Dostoevsky", 796, false);
 addBookToLibrary("1984", "George Orwell", 368, true);
 addBookToLibrary("The Handmaid's Tale", "Margaret Atwood", 311, true);
+
+fillLibrary();

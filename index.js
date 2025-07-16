d454711afeae93b3fe3475358bbe80a9aa5a5d0a
Andrei Valdez <65985776+andreivaldezau@users.noreply.github.com>
2025-07-16 22:36:17 +1000

    Add toggleRead and removeBook buttons

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const myLibrary = [];
+let myLibrary = [];
 const container = document.querySelector(".container");
 
-function Book(title, author, pages, is_read) {
+function Book(title, author, pages, isRead) {
   if (!new.target) {
     throw Error("Syntax error: initialise with 'new' operator");
   }
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.is_read = is_read;
+  this.isRead = isRead;
   this.id = self.crypto.randomUUID();
 }
 
 Book.prototype.info = function () {
   string = `${this.title} by ${this.author}, ${this.pages} pages, `;
-  string += this.is_read === true ? "read" : "not read yet";
+  string += this.isRead === true ? "read" : "not read yet";
   return string;
 };
 
-function addBookToLibrary(title, author, pages, is_read) {
-  book = new Book(title, author, pages, is_read);
+function addBookToLibrary(title, author, pages, isRead) {
+  book = new Book(title, author, pages, isRead);
   myLibrary.push(book);
 }
 
 function addCard(book) {
   const card = document.createElement("div");
   card.classList.add("card");
+  if (book.isRead === true) {
+    card.classList.add("read");
+  } else {
+    card.classList.add("not-read");
+  }
+
   const title = document.createElement("h2");
   title.textContent = `${book.title}`;
   const author = document.createElement("p");
@@ -33,14 +39,44 @@ function addCard(book) {
   author.classList.add("italic");
   const details = document.createElement("p");
   details.textContent = `${book.pages} pages`;
-  if (book.is_read === true) {
-    card.classList.add("read");
-  } else {
-    card.classList.add("not-read");
-  }
+  const controls = document.createElement("div");
+  controls.classList.add("controls");
+
+  const toggleIsRead = document.createElement("button");
+  toggleIsRead.textContent = "Toggle Read";
+  toggleIsRead.addEventListener("click", () => {
+    const id = card.dataset.id;
+    myLibrary.forEach((book) => {
+      if (book.id === id) {
+        book.isRead = !book.isRead;
+      }
+    });
+    if (book.isRead === true) {
+      card.classList.add("read");
+      card.classList.remove("not-read");
+    } else {
+      card.classList.add("not-read");
+      card.classList.remove("read");
+    }
+  });
+
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Remove";
+  removeBtn.addEventListener("click", () => {
+    const id = card.dataset.id;
+    myLibrary = myLibrary.filter((book) => {
+      return book.id !== id;
+    });
+    card.remove();
+  });
+
+  card.setAttribute("data-id", `${book.id}`);
   card.appendChild(title);
   card.appendChild(author);
   card.appendChild(details);
+  card.appendChild(controls);
+  controls.appendChild(toggleIsRead);
+  controls.appendChild(removeBtn);
   return container.appendChild(card);
 }
 
@@ -50,6 +86,8 @@ function addBookButton() {
   card.textContent = "Add Book";
   return container.appendChild(card);
 }
+
+function toggleReadStatus(element) {}
 
 function fillLibrary() {
   myLibrary.forEach((book) => {
